c2e547d3807b8c8daaa3c8ed9d0192f018c45930
Marcus Vinicius <mvsantana67@gmail.com>
2023-01-29 16:01:42 -0300

    aulas iniciasis

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double salarioMinimo = 2.5; // é igual a 2,5 e não ígual a 2500 reais.
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // isso é um cast
+
+        int numero = 1;
+        numero = 2;
+
+        //VALOR NUNCA MUDA QUANDO O TORNAMOS UMA CONSTANTE
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
