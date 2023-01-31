efa6896d2aa3acff728681df748b725d6b070681
Marcus Vinicius <mvsantana67@gmail.com>
2023-01-31 19:38:58 -0300

    operadores

---
+++ src/OperadorTernario.java
@@ -0,0 +1,2 @@
+public class OperadorTernario {
+}
---
+++ src/OperadorUnario.java
@@ -0,0 +1,13 @@
+public class OperadorUnario {
+    public static void main(String[] args) {
+        boolean variavel = true;
+
+        //repeticao
+
+        variavel = !variavel;
+        System.out.println(variavel);
+        System.out.println(variavel);
+
+
+    }
+}
