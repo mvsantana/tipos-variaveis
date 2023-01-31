6fa30164ab56955051aab438f188378a7f8a6ef3
Marcus Vinicius <mvsantana67@gmail.com>
2023-01-31 20:12:46 -0300

    operadores

--- src/OperadorTernario.java
+++ src/OperadorTernario.java
@@ -1,2 +1,13 @@
 public class OperadorTernario {
+    public static void main(String[] args) {
+
+
+        int a, b;
+        a = 6;
+        b = 6;
+
+        int resultado = a == b ? 1 : 0;
+
+        System.out.println(resultado);
+    }
 }
---
+++ src/OperadoresRelacionais.java
@@ -0,0 +1,16 @@
+public class OperadoresRelacionais {
+
+    public static void main(String[] args) {
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("Número é igual a numero Dois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("Número é igual a numero Dois? " + simNao);
+    }
+}
