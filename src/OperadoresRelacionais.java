b2121e45811c7fbf5df2e039d9ffd9d53010655c
Marcus Vinicius <mvsantana67@gmail.com>
2023-01-31 20:27:19 -0300

    operadores

--- src/OperadoresRelacionais.java
+++ src/OperadoresRelacionais.java
@@ -2,15 +2,33 @@ public class OperadoresRelacionais {
 
     public static void main(String[] args) {
 
+        String nomeUm = "GLEYSON";
+        String nomedOIS = new String("GLEYSON");
+
+        System.out.println(nomeUm.equals(nomedOIS));
+
         int numero1 = 1;
         int numero2 = 2;
 
         boolean simNao = numero1 == numero2;
 
+        if (numero1 > numero2) {
+            System.out.println("A nossa condição é verdadeira");
+        } else
+            System.out.println("A condição é falsa");
+
         System.out.println("Número é igual a numero Dois? " + simNao);
 
         simNao = numero1 != numero2;
 
-        System.out.println("Número é igual a numero Dois? " + simNao);
+        System.out.println("Número é diferente do numero Dois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("Número é maior do que o numero Dois? " + simNao);
+
+        simNao = numero1 < numero2;
+
+        System.out.println("Número é menor do que o numero Dois? " + simNao);
     }
 }
